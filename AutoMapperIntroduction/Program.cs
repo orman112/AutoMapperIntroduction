04d9ef1ebf88b201476eaf3d5592522652871bc0
Clayton Orman <COrman@jandlmarketing.com>
2017-09-18 08:43:35 -0400

    Add project files.

---
+++ AutoMapperIntroduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoMapperIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
